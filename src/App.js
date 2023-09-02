836a7f06ff3d7bfa9e1f6c15f1404eaf6de57624
pinocchio-life-like <icbr19fl@gmail.com>
2023-09-02 15:45:32 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom"; // Import Routes and Route
 
 import Home from "./Components/Home/Home";
 import TheStudio from "./Components/TheStudio/TheStudio";
+import Services from "./Components/Services/Services";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/theStudio" element={<TheStudio />} />
+      <Route path="/services" element={<Services />} />
     </Routes>
   );
 }
---
+++ src/Components/Services/Services.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "./Services.css";
+import Header from "../Header/Header";
+
+function Services() {
+  return (
+    <div>
+      <div className="ServicesBannerClass">
+        <Header />
+      </div>
+      <div className="ServicesBannerContainer">
+        <div className="ServicesBannerHeader">
+          <div
+            className="ServicesCaption"
+            style={{ color: "white", bottom: 0 }}>
+            Explore Our Studio
+          </div>
+          <div className="ServicesHeaderText">Hair services</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Services;
