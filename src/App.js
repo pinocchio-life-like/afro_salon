ed792d0dcb3965cfe00480b6e066ca3daceca04b
pinocchio-life-like <icbr19fl@gmail.com>
2023-09-14 16:53:59 +0300

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom"; // Import Routes and Route
 import Home from "./Components/Home/Home";
 import TheStudio from "./Components/TheStudio/TheStudio";
 import Services from "./Components/Services/Services";
+import Book from "./Components/Book/Book";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/theStudio" element={<TheStudio />} />
       <Route path="/services" element={<Services />} />
+      <Route path="/bookNow" element={<Book />} />
     </Routes>
   );
 }
---
+++ src/Components/Book/Book.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Book.css";
+import logoImage from "../../Images/Google-light.3ac01ebb.svg";
+
+const Book = () => {
+  return (
+    <div className="BookNowClass">
+      <button className="SignInButton">
+        <img
+          alt="Google logo"
+          loading="lazy"
+          width="20"
+          height="20"
+          decoding="async"
+          style={{ color: "transparent", marginRight: "10px" }}
+          src={logoImage}
+        />
+        Sign in with Google
+      </button>
+    </div>
+  );
+};
+
+export default Book;
